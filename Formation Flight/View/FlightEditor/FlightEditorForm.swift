d092c324d8b3f85280ba6fe73ba9a1ac8314e48e
Jack Ellis <jack@mnmlst.cc>
2025-10-19 18:20:25 -0700

    Enable reordering of Checkpoints in FlightEditorForm view

--- Formation Flight/View/FlightEditor/FlightEditorForm.swift
+++ Formation Flight/View/FlightEditor/FlightEditorForm.swift
@@ -13,51 +13,55 @@ struct FlightEditorForm: View {
     @State var checkPointPopover = false
     
     var body: some View {
-        Form{
-            Section(header: Text("Flight Overview")) {
-                TextField("Mission Title", text: $config.flight.title)
-                DatePicker("Mission Date", selection: $config.flight.missionDate, displayedComponents: [.date, .hourAndMinute])
-            }
-            Section("Wind Conditions") {
-                HStack {
-                    Text("Direction:").fontWeight(.thin)
-
-                    TextField(value: $config.flight.expectedWinds.directionAsDegrees,
-                              formatter: windInputFormatter,
-                              prompt: Text("Wind Direction")) {
-                        Text("Wind Direction")
-                    }
-                          .keyboardType(.numberPad)
+        NavigationStack {
+            Form{
+                Section(header: Text("Flight Overview")) {
+                    TextField("Mission Title", text: $config.flight.title)
+                    DatePicker("Mission Date", selection: $config.flight.missionDate, displayedComponents: [.date, .hourAndMinute])
                 }
-
-                HStack {
-                    Text("Velocity:  ").fontWeight(.thin)
-
-                    TextField("Wind Velocity",
-                              value: $config.flight.expectedWinds.velocityAsKnots,
-                              formatter: windInputFormatter)
-                    .keyboardType(.numberPad)
-                }
-            }
-            Section("Flight Plan") {
-                ForEach(config.flight.checkPoints) { checkPoint in
+                Section("Wind Conditions") {
                     HStack {
-                        Text(checkPoint.name)
-                        Spacer()
-                        VStack {
-                            Text("\(checkPoint.longitude)")
-                            Text("\(checkPoint.latitude)")
+                        Text("Direction:").fontWeight(.thin)
+
+                        TextField(value: $config.flight.expectedWinds.directionAsDegrees,
+                                  formatter: windInputFormatter,
+                                  prompt: Text("Wind Direction")) {
+                            Text("Wind Direction")
+                        }
+                        .keyboardType(.numberPad)
+                    }
+
+                    HStack {
+                        Text("Velocity:  ").fontWeight(.thin)
+
+                        TextField("Wind Velocity",
+                                  value: $config.flight.expectedWinds.velocityAsKnots,
+                                  formatter: windInputFormatter)
+                        .keyboardType(.numberPad)
+                    }
+                }
+                Section("Flight Plan") {
+                    ForEach(config.flight.checkPoints) { checkPoint in
+                        HStack {
+                            Text(checkPoint.name)
+                            Spacer()
+                            VStack(alignment: .trailing) {
+                                Text("\(checkPoint.longitude)")
+                                Text("\(checkPoint.latitude)")
+                            }
                         }
                     }
+                    .onDelete(perform: { indexSet in
+                        config.flight.checkPoints.remove(atOffsets: indexSet)
+                    })
+                    .onMove(perform: { indices, newOffset in
+                        config.flight.checkPoints.move(fromOffsets: indices, toOffset: newOffset)
+                    })
+                    FlightEditorCheckPoint(flight: config.flight)
                 }
-                .onDelete(perform: { indexSet in
-                    config.flight.checkPoints.remove(atOffsets: indexSet)
-                })
-                .onMove(perform: { indices, newOffset in
-                    config.flight.checkPoints.move(fromOffsets: indices, toOffset: newOffset)
-                })
-                FlightEditorCheckPoint(flight: config.flight)
             }
+            .navigationTitle("Edit Flight")
+            .toolbar { EditButton() }
         }
     }
  
@@ -73,4 +77,3 @@ struct FlightEditorForm: View {
 #Preview {
     FlightEditorForm(config: .constant(FlightEditorConfig()))
 }
-
